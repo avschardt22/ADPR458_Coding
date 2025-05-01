e071abd61ddc785d592522082dc0a487b65070c1
Avryn Schardt <av.schardt@gmail.com>
2025-05-01 10:20:41 -0500

    form

--- js/script.js
+++ js/script.js
@@ -4,3 +4,37 @@ const navListItems = document.getElementById("nav-menu-list");
 toggleBtn.addEventListener("click", function () {
   navListItems.classList.toggle("active");
 });
+
+$(".slider").slick({
+
+  // normal options...
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 5000
+  pauseOnfocus: false
+
+  // the magic
+  responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+        autoplay: true
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+
+    }, {
+
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+
+    }]
+});
